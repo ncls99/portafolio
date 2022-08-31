9965612a87c3b8a96bbb72dfd0ab622296887eed
ncls99 <junico99301@gmail.com>
2022-08-30 22:28:43 -0500

    Ocultar contex menu en cada a

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
+const listaMenus = document.getElementsByClassName("hideMenu") 
+console.log(listaMenus)
 const contextMenu = document.querySelector("#hamburguer")
 console.log(contextMenu)
 const menu = document.querySelector("#menuDesp")
 
-contextMenu.addEventListener("click", () => {
+function showMenu() {
     menu.classList.toggle("menuHamCon");
-})
+}
+
+contextMenu.addEventListener("click", showMenu )
+
+for (let i in listaMenus){
+    listaMenus[i].addEventListener("click", showMenu)
+}
